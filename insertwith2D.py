01af1294e2ebff32f75c028f0fb17a9ce3f23e0b
Sudharkar <sudhakarchaturvedi9329@gmail.com>
2025-08-26 08:12:37 -0700

    Delete insertwith2D.py

--- insertwith2D.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-arr=np.array([[4,5,54],[6,7,7]])
-new=np.insert(arr,1,[7,8,8],axis=None)
-print(new)
-
-arr=np.array([[4,5,54],[6,7,7]])
-new=np.insert(arr,1,[7,8,8],axis=1)
-print(new)
