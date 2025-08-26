7ed17356d523b7328f36529dfd72ef04a135ee81
Sudharkar <sudhakarchaturvedi9329@gmail.com>
2025-08-26 08:12:15 -0700

    Delete hist.py

--- hist.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-scores=[45,68,84,75,66,95,89,61,82,94,97,91,55,53,55,67,66]
-plt.hist(scores,bins=5,color='purple',edgecolor='black')
-plt.xlabel('score range',color="blue")
-plt.ylabel("Number of student",color="blue")
-plt.title("Score Distribution Of Student",color="orange")
-plt.show()
